2424e05e6a4a2f401e27d6093ecd631573d3d692
zhaomsdemo <zhaomsdemo@gmail.com>
2023-01-28 15:01:49 -0800

    init

---
+++ src/main/java/com/jianhuazhao6/ecom/customeraccountbes/CustomerAccountBesApplication.java
@@ -0,0 +1,13 @@
+package com.jianhuazhao6.ecom.customeraccountbes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAccountBesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerAccountBesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jianhuazhao6/ecom/customeraccountbes/CustomerAccountBesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jianhuazhao6.ecom.customeraccountbes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerAccountBesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
